c9804489bd91aa7361090ce9690ea4d120fe326a
7demo <liyanan7demo@gmail.com>
2015-09-09 20:57:22 +0800

    create

---
+++ fis-conf.js
@@ -0,0 +1,57 @@
+fis.match('::packager', {
+	spriter : fis.plugin('csssprites')
+	//不采用暴力打包
+	// ,
+	// postpackager: fis.plugin('loader', {
+	//     allInOne : 'css'
+	// })
+});
+
+/**
+ * sass的解析  
+ */
+fis.match('/lib/css/(*).scss', {
+	rExt : '.css',
+	parser : fis.plugin('sass'), //加载sass插件
+	useSprite : true, //使用雪碧图
+	optimizer: fis.plugin('clean-css') //压缩css
+})
+
+/**
+ * 合并多个css 并发布合并后的css
+ */
+fis.match('/lib/css/(*).scss', {
+	packTo : '/Public/css/output/pak.css',
+	release : '/Public/css/output/pak.css',
+	url : '/Public/css/output/pak.css'
+})
+
+/**
+ * js的解析
+ */
+fis.match('/lib/js/(*).js', {
+	optimizer : fis.plugin('uglify-js'),
+	release : '/Public/js/$1'
+})
+
+
+/**
+ * 图片的解析
+ */
+fis.match('/lib/images/(*).{jpg,jpeg,png,gif}', {
+	optimizer : fis.plugin('png-compressor'),
+	release : '/Public/images/$1'
+});
+fis.match(/\/lib\/css\/output\/(.*_[xyz])\.png$/, {
+  release: '/Public/images/$1'
+});
+
+// fis.media('debug').match('**/**.{js,css,png}', {
+// 	useHash : false,
+// 	useSprite : false,
+// 	optimizer : null
+// })
+
+// fis.match('*.{css,less}', {
+//   packTo: '/static/aio.css'
+// });
