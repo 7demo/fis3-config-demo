a9131c33bc0e833528f61df7a40458f245a55247
7demo <liyanan7demo@gmail.com>
2015-09-10 19:10:51 +0800

    不带模块化js配置

--- fis-conf.js
+++ fis-conf.js
@@ -1,60 +1,55 @@
+/**
+ * js 模块化开发
+ */
+fis.hook('amd', {
+	
+})
+
+
 fis.match('::packager', {
 	spriter : fis.plugin('csssprites')
-	//不采用暴力打包
-	// ,
-	// postpackager: fis.plugin('loader', {
-	//     allInOne : 'css'
-	// })
 });
 
 /**
  * sass的解析  
+ * 关于样式的合并，直接在开发的时候使用sass 的import 进行引用合并 ，不再后期进行暴力打包合并
+ * 分通用功能common.sass与具体功能样式
  */
 fis.match('/lib/css/(*).scss', {
 	rExt : '.css',
 	parser : fis.plugin('sass'), //加载sass插件
 	useSprite : true, //使用雪碧图
-	optimizer: fis.plugin('clean-css') //压缩css
-})
-
-/**
- * 合并多个css 并发布合并后的css
- * 采用开发时候，直接使用sass 的import 进行引用 ，不后期进行打包
- * 分通用功能common.sass与具体功能样式
- * 
- */
-fis.match('/lib/css/(*).scss', {
-	packTo : '/Public/css/output/pak.css',
-	release : '/Public/css/output/pak.css',
-	url : '/Public/css/output/pak.css'
+	optimizer: fis.plugin('clean-css'), //压缩css
+	release : '/Public/css/$1'
 })
 
 /**
  * js的解析
+ * 直接进行压缩
  */
 fis.match('/lib/js/(*).js', {
-	optimizer : fis.plugin('uglify-js'),
+	// optimizer : fis.plugin('uglify-js'),
 	release : '/Public/js/$1'
-})
+});
+fis.match('/lib/js/modules/(*).js', {
+    isMod: true, // 设置 comp 下都是一些组件，组件建议都是匿名方式 define
+    // release: '/Public/js/modules/$1'
+});
+
+
 
 
 /**
- * 图片的解析
+ * 图片的解析，发布
+ * 包括css中压缩出现的图片
  */
-fis.match('/lib/images/(*).{jpg,jpeg,png,gif}', {
+fis.match(/^\/lib\/(css|images)\/(.*)\.(jpg|gig|jpeg|png)$/, {
 	optimizer : fis.plugin('png-compressor'),
-	release : '/Public/images/$1'
-});
-fis.match(/\/lib\/css\/output\/(.*_[xyz])\.png$/, {
-  release: '/Public/images/$1'
-});
+	release: '/Public/images/$2'
+})
 
 // fis.media('debug').match('**/**.{js,css,png}', {
 // 	useHash : false,
 // 	useSprite : false,
 // 	optimizer : null
 // })
-
-// fis.match('*.{css,less}', {
-//   packTo: '/static/aio.css'
-// });
